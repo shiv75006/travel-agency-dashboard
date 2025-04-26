ac328410fad6a2b697f30b4e84e1b2054a0dfd4f
Shivyanshu <94912395+shiv75006@users.noreply.github.com>
2025-04-26 17:22:44 +0530

    routing

---
+++ app/routes/admin/all-users.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const AllUsers = () => {
+    return (
+        <div>AllUsers Table</div>
+    )
+}
+export default AllUsers
--- app/routes/home.tsx
+++
@@ -1,7 +0,0 @@
-const Home = () => {
-  return (
-    <div>Home Pge</div>
-  )
-}
-
-export default Home
